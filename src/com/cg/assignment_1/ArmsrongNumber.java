e20b262c8b32772dcbd8c66bde9622086a5ad117
vishaltawar <vishalmtawar0205@gmail.com>
2022-06-18 14:03:57 +0530

    Armstrong Number Between 100-999

--- src/com/cg/assignment_1/ArmsrongNumber.java
+++ src/com/cg/assignment_1/ArmsrongNumber.java
@@ -16,23 +16,48 @@ public class ArmsrongNumber {
 		}	
 		if(arm==no1)
 		{
-			result="Number is Armstrong";
+			result=no1 +" is Armstrong";
 		}
 		else
 		{ 
-			result="Number is Not Armstrong";
+			result=no1+" is Not Armstrong";
 		}
 		return result;
 	}
+	
+		public void armstrongNoRange()
+		{
+			int i,r,t,arm=0,fn,sn,tn;
+			for(i=100;i<=999;i++)
+			{
+				t=i;
+				
+				fn=t%10;
+				sn=(t/10)%10;
+				tn=(t/100)%10;
+				arm=(fn*fn*fn)+(sn*sn*sn)+(tn*tn*tn);
+				
+				if(arm==i)
+				{
+					System.out.println(i);
+					
+				}
+			}
+		}
+
+	
+	
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the Number :-");
 		int no1= sc.nextInt();
 		ArmsrongNumber a =new ArmsrongNumber();
-		
 		System.out.println(a.armstrongNo(no1));
+		System.out.println();
+		System.out.println("------------  Armstrong Number Between 100 to 999  -------------------------");
 		
+		a.armstrongNoRange();
 	}
 	
 }
